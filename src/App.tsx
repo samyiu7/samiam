90f1493a6600a13f2aa0c999e03a99f8c65b2258
samyiu7 <75868457+samyiu7@users.noreply.github.com>
2022-10-18 20:39:57 -0700

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className={'bg-lime-500'}>
+        HELLO MY NAME IS SAMIAM
+    </div>
+  )
+}
+
+export default App
